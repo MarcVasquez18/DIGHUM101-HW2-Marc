a819aa43e6db8093b13fe0dc150d28b879e5952a
MarcVasquez18 <39502031+MarcVasquez18@users.noreply.github.com>
2018-06-19 15:31:11 -0700

    Add files via upload

---
+++ assistant.sh
@@ -0,0 +1,36 @@
+echo "Hello, Marc! My name is Codemaster, and I'm your personal assistant. Carpe diem!"
+
+printf "\n"
+
+echo "1. Give your assistant a name, and have it greet you with a welcome message."
+
+printf "\n"
+
+echo "2. Add the current time and date with the date function."
+
+date
+
+printf "\n"
+
+echo "3. Add the calendar with the cal function."
+
+printf "\n"
+
+cal
+
+printf "\n"
+
+echo "4. Add insructions for ten tips that your personal assistant can give you at startup. Below are three examples to get you started:"
+
+printf "\n"
+
+echo "Tip 1. Type jupyter notebook to open a jupyter notebook."
+echo "Tip 2. Type screencapture filename.png to take a screenshot."
+echo "Tip 3. Type open somefile.ext to open it."
+echo "Tip 4. Type whoami to show who the current user is."
+echo "Tip 5. Type ls to list the contents of the working directory."
+echo "Tip 6. Type touch to create a file."
+echo "Tip 7. Type rm to delete."
+echo "Tip 8. Type pwd to show the current working directory."
+echo "Tip 9. Type mkdir to make a directory."
+echo "Tip 10. Type cd / to change your working directory to the root directory."
